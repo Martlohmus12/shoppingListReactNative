1a8e57e7be7904565eeb70f9f5791ad0f7e412b9
Semjon Greef <martlohmus12@gmail.com>
2018-02-19 17:13:13 +0200

    refresh list after actions

--- src/actions/shoppingList/index.js
+++ src/actions/shoppingList/index.js
@@ -1,21 +1,6 @@
 import axios from 'axios';
 
 export const RECEIVE_ALL_ITEMS = 'RECEIVE_ALL_ITEMS';
-// export const PANEL_IS_LOADING = 'PANEL_IS_LOADING';
-
-export function haveErrors(bool) {
-  return {
-    type: 'PANEL_HAVE_ERRORS',
-    haveErrors: bool
-  };
-}
-
-export function isLoading(bool) {
-  return {
-    type: 'PANEL_IS_LOADING',
-    isLoading: bool
-  };
-}
 
 export function receiveAllItems(items) {
   return {
@@ -24,59 +9,49 @@ export function receiveAllItems(items) {
   };
 }
 
-export function itemsFetchData(url) {
+export function fetchItems(url) {
   return (dispatch) => {
     fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response;
-    })
-    .then((response) => response.json())
-    .then(function (items) {
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then(function (items) {
         dispatch(receiveAllItems(items));
-    })
-    .catch((error) => dispatch(haveErrors(error)));
+      })
+      .catch((error) => dispatch(haveErrors(error)));
   };
 }
 
 export function newItem(url, action) {
-    return function (dispatch) {
-        axios.post(`${url}`, action)
-        .then(response => {
-            dispatch({
-                type: "RECEIVE_NEW_ITEM",
-                payload: response.data
-            })
-        })
-    }
+  return function (dispatch) {
+    axios.post(`${url}`, action)
+      .then(response => {
+        dispatch(fetchItems('http://localhost:3000/api/items'));
+      })
+  }
 }
 
-export function toggleStatus(url, id) {
-    const _url = url + "/" + id;
-    console.log(_url);
-    return function (dispatch) {
-        axios.put(`${_url}`)
-        .then(response => {
-            dispatch({
-                type: "TOGGLE_STATUS",
-                payload: response.data
-            })
-        })
-    }
+export function toggleStatus(url, id, status) {
+  const _url = url + "/" + id + "/" + status;
+  return function (dispatch) {
+    axios.put(`${_url}`)
+      .then(response => {
+        dispatch(fetchItems('http://localhost:3000/api/items'));
+      })
+  }
 }
 
 export function deleteItem(url, id) {
-    const _url = url + "/" + id;
-    console.log(_url);
-    return function (dispatch) {
-        axios.delete(`${_url}`, null)
-        .then(response => {
-            dispatch({
-                type: "ITEM_DELETED",
-                payload: response.data
-            })
-        })
-    }
+  const _url = url + "/" + id;
+  return function (dispatch) {
+    axios.delete(`${_url}`, null)
+      .then(response => {
+        dispatch(fetchItems('http://localhost:3000/api/items'));
+      })
+  }
 }
+
